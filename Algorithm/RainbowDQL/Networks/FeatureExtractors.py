08cd2943a72004b8062b3618e276464d559746c1
dsdiop <dameseck121200@gmail.com>
2023-04-27 04:29:44 +0200

    Fifth commit-2: DQF second implementation(one network with 5 types of feature extractor - one optimization with the sum of loss unweighted) - reward explorations v1

--- Algorithm/RainbowDQL/Networks/FeatureExtractors.py
+++ Algorithm/RainbowDQL/Networks/FeatureExtractors.py
@@ -6,7 +6,7 @@ import numpy as np
 import sys
 import os
 sys.path.append(os.path.join(os.path.dirname(__file__)))
-from cbam import CBAMBlock
+from cbam import CBAMBlock, CBAMBlock_w_cnn
 """
 class FeatureExtractor(nn.Module):
     """""" Convolutional Feature Extractor for input images """"""
