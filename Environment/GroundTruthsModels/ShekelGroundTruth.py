66ba2988b9b1a71f019a242d38d00d1d6c263d1e
dsdiop <dameseck121200@gmail.com>
2023-09-28 11:18:07 +0200

    Fixed shekelgroundtruth (by Alejandro)

--- Environment/GroundTruthsModels/ShekelGroundTruth.py
+++ Environment/GroundTruthsModels/ShekelGroundTruth.py
@@ -10,7 +10,7 @@ class GroundTruth(object):
 
     """ Ground Truth generator class.
         It creates a ground truth within the specified navigation map.
-        The ground truth is generated randomly following some realistic rules of the enviornment
+        The ground truth is generated randomly following some realistic rules of the enqqqviornment
         and using a Shekel function.
     """
 
@@ -19,19 +19,21 @@ class GroundTruth(object):
         """ Maximum number of peaks encountered in the scenario. """
         self.max_number_of_peaks = 6 if max_number_of_peaks is None else max_number_of_peaks
         self.seed = seed
-        np.random.seed(self.seed)
+        self.rng = np.random.default_rng(seed=self.seed) # random number generator, it's better than set a np.random.seed() (https://builtin.com/data-science/numpy-random-seed)
 
         """ random map features creation """
         self.grid = 1.0 - grid
-        self.xy_size = np.array([self.grid.shape[1]/self.grid.shape[0]*10, 10])
+        self.xy_size = np.asarray(grid.shape)
+        self.xy_size = np.flip(self.xy_size)
         self.is_bounded = is_bounded
         self.dt = 0.01
 
         # Peaks positions bounded from 1 to 9 in every axis
-        self.number_of_peaks = np.random.randint(1, self.max_number_of_peaks+1)
-        self.A = np.random.rand(self.number_of_peaks, 2) * self.xy_size * 0.8 + self.xy_size*0.2
+        self.number_of_peaks = self.rng.integers(1, self.max_number_of_peaks+1)
+        self.A = self.rng.random((self.number_of_peaks, 2)) * self.xy_size
         # Peaks size bounded from a minimum 2.5 to 5
-        self.C = np.random.rand(self.number_of_peaks, 1) + 0.5
+        self.C = self.rng.random((self.number_of_peaks, 1)) + 0.5
+        # # self.C = np.random.rand(self.number_of_peaks, 1) + 0.5
 
         """ Creation of the map field """
         self._x = np.arange(0, self.grid.shape[1], 1)
@@ -45,7 +47,7 @@ class GroundTruth(object):
     def shekel_arg0(self, sol):
 
         return np.nan if self.grid[sol[1]][sol[0]] == 1 else \
-            benchmarks.shekel(sol[:2]/np.array(self.grid.shape)*10, self.A, self.C)[0]
+            benchmarks.shekel(sol[:2], self.A, self.C)[0]
 
     def create_field(self):
 
@@ -65,13 +67,16 @@ class GroundTruth(object):
             self.normalized_z = np.nan_to_num(self.normalized_z, nan=np.nanmin(self.normalized_z))
             self.normalized_z = (self.normalized_z - np.min(self.normalized_z))/(np.max(self.normalized_z) - np.min(self.normalized_z))
 
+        #self.normalized_z = self.normalized_z.T
+
+
     def reset(self):
         """ Reset ground Truth """
         # Peaks positions bounded from 1 to 9 in every axis
-        self.number_of_peaks = np.random.randint(1,self.max_number_of_peaks+1)
-        self.A = np.random.rand(self.number_of_peaks, 2) * self.xy_size * 0.9 + self.xy_size*0.1
+        self.number_of_peaks = self.rng.integers(1,self.max_number_of_peaks+1)
+        self.A = self.rng.random((self.number_of_peaks, 2)) * self.xy_size
         # Peaks size bounded from a minimum 2.5 to 5
-        self.C = np.random.rand(self.number_of_peaks, 1) + 0.5
+        self.C = 10*(self.rng.random((self.number_of_peaks, 1)) + 0.5)
         # Reconstruct the field #
         self.create_field()
 
@@ -91,14 +96,15 @@ class GroundTruth(object):
         cs = plt.contour(self.read(), colors='royalblue', alpha=1, linewidths=1)
         plt.clabel(cs, inline=1, fontsize=7)
         plt.title("Nº of peaks: {}".format(gt.number_of_peaks), color='black', fontsize=10)
-        im = plt.plot(self.A[:, 0]*self.grid.shape[0]/10,
-                      self.A[:, 1]*self.grid.shape[1]/10, 'hk', )
+        im = plt.plot(self.A[:, 0],
+                      self.A[:, 1], 'hk', )
         plt.show()
 
     def step(self):
         """ Move every maximum with a random walk noise """
 
-        self.A += self.dt*(2*(np.random.rand(*self.A.shape)-0.5) * self.xy_size * 0.9 + self.xy_size*0.1)
+        self.A += self.dt*(2*(self.rng.random(*self.A.shape)-0.5) * self.xy_size * 0.9 + self.xy_size*0.1)
+        # self.A += self.dt*(2*(np.random.rand(*self.A.shape)-0.5) * self.xy_size * 0.9 + self.xy_size*0.1)
         self.create_field()
 
         pass
@@ -108,13 +114,22 @@ if __name__ == "__main__":
 
     import matplotlib.pyplot as plt
 
-    ypacarai_map = np.genfromtxt('Environment/Maps/example_map.csv',delimiter=',',dtype=float)
+    ypacarai_map = np.genfromtxt('Environment/Maps/ypacarai_map_low_res.csv',delimiter=',',dtype=float)
     gt = GroundTruth(ypacarai_map, max_number_of_peaks=6, is_bounded=True, seed=10)
 
-    for i in range(10):
+    for i in range(100):
         gt.reset()
         gt.render()
 
+    # A_total = np.empty((0, 2), float)
+
+    # for i in range(1000):
+    #     gt.reset()
+    #     A_total = np.vstack((A_total,gt.A))
+    
+    # plt.imshow(ypacarai_map)
+    # im = plt.plot(A_total[:, 0], A_total[:, 1], 'rx', )
+    # plt.show()
 
 
 
